a69ca89177ec552060551bb765008345ebe4b2e1
s-evg <s-evg@bk.ru>
2021-11-28 11:58:55 +0500

    Исправление найденых ошибок

--- synonym/views.py
+++ synonym/views.py
@@ -23,7 +23,6 @@ def get_synonym_view(request):
 
     word = request.GET.get("word")
     template_name = 'synonym/index.html'
-    print(f'WORD ===>>> {word}')
     if word is None:
         context = {
             'info_1': INFO_1,
